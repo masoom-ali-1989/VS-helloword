25fee297a9f6b941d7ebe05406a52c61b3163984
Masoom Ali <47391403+masoom-ali-1989@users.noreply.github.com>
2023-07-27 01:54:33 +0500

    commiting Hello Word

---
+++ Pictures/testproject/testCode/helloword/helloword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, We are looking someone he is Good Friend");
+            Console.WriteLine("Hello, World! I Hop This is Good Days");
+
+            Console.ReadKey();
+        }
+    }
+}
